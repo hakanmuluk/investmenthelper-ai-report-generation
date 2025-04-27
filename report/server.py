c383ffea78fe199278f66a20cd90d23a2710f950
Hakan Muluk <91740172+hakanmuluk@users.noreply.github.com>
2025-04-28 00:15:11 +0300

    Delete report/server.py

--- report/server.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-resp = requests.post(
-    "http://localhost:8000/generate-report",
-    json={"reportGenerationQuery":"Turkcell'in son 3 aydaki finansal durumunu incele", "username": "tadic@gmail.com"}
-)
-print(resp.json()["report"])
